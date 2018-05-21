127782d658648ee5990aeecfc1f55d8ad480f82f
Maksym Vorobiov <maksim.vorobyov@bigdropinc.com>
2018-05-21 17:45:08 +0300

    new files

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
+import Login from './components/register/auth/Login';
+import Register from './components/register/auth/Register';
+
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <Navbar/>
+                    <Route path="/" component={Landing} exact/>
+                    <div className="container">
+                        <Route exact path="/register" component={Register}/>
+                        <Route exact path="/login" component={Login}/>
+                    </div>
+                    <Footer/>
+                </div>
+            </Router>
+        );
+    }
+}
+
+export default App;
