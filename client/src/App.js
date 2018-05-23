a2e3eb61ca6db632acfea900287cd573cf24ccf2
maksimvorobyov <gtl.vorobyov@gmail.com>
2018-05-24 01:18:29 +0300

    create

--- client/src/App.js
+++ client/src/App.js
@@ -16,52 +16,55 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/create-profile/CreateProfile';
 
 import './App.css';
 
 // Check for token
 if (localStorage.jwtToken) {
-    // Set auth token header auth
-    setAuthToken(localStorage.jwtToken);
-    // Decode token and get user info and exp
-    const decoded = jwt_decode(localStorage.jwtToken);
-    // Set user and isAuthenticated
-    store.dispatch(setCurrentUser(decoded));
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
 
-    //Check for expired token
-    const currentTime = Date.now() / 1000;
-    if (decoded.exp < currentTime) {
-        // Logout user
-        store.dispatch(logoutUser());
-        // Clear current Profile
-        store.dispatch(clearCurrentProfile());
-        // Redirect to login
-        window.location.href = '/login';
-    }
+  //Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Clear current Profile
+    store.dispatch(clearCurrentProfile());
+    // Redirect to login
+    window.location.href = '/login';
+  }
 }
 
 class App extends Component {
-    render() {
-        return (
-            <Provider store={store}>
-                <Router>
-                    <div className="App">
-                        <Navbar/>
-                        <Route path="/" component={Landing} exact/>
-                        <div className="container">
-                            <Route exact path="/register" component={Register}/>
-                            <Route exact path="/login" component={Login}/>
-                            <Switch>
-
-                                <PrivateRoute exact path="/dashboard" component={Dashboard}/>
-                            </Switch>
-                        </div>
-                        <Footer/>
-                    </div>
-                </Router>
-            </Provider>
-        );
-    }
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar/>
+            <Route path="/" component={Landing} exact/>
+            <div className="container">
+              <Route exact path="/register" component={Register}/>
+              <Route exact path="/login" component={Login}/>
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard}/>
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/create-profile" component={CreateProfile}/>
+              </Switch>
+            </div>
+            <Footer/>
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
