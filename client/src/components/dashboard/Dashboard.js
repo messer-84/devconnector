49c57dc38ee413cf3c8e0824e414aa8676733f04
Maksym Vorobiov <maksim.vorobyov@bigdropinc.com>
2018-05-24 18:50:55 +0300

    react add

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import propTypes from 'prop-types';
 import {connect} from 'react-redux';
-import {getCurrentProfile} from "../../actions/profileActions";
+import {getCurrentProfile, deleteAccount} from "../../actions/profileActions";
 import Spinner from '../common/Spinner';
+import ProfileActions from './ProfileActions';
 
 class Dashboard extends Component {
     componentDidMount() {
         this.props.getCurrentProfile();
+    }
+
+    onDeleteClick(e){
+        this.props.deleteAccount();
     }
 
     render() {
@@ -17,35 +22,49 @@ class Dashboard extends Component {
         let dashboardContent;
 
         if (profile === null || loading) {
-            dashboardContent = <Spinner />;
+            dashboardContent = <Spinner/>;
         } else {
             // Check if logged in user has profile data
-            if(Object.keys(profile).length > 0){
-                dashboardContent = <h4>TODO: DISPLAY PROFILE</h4>
-            } else{
+            if (Object.keys(profile).length > 0) {
+                dashboardContent = (
+                    <div>
+                        <p className="lead text-muted">Welcome <Link
+                            to={`/profile/${profile.handle}`}>{user.name}</Link></p>
+                        <ProfileActions/>
+                        {/* TODO: exp and edu*/}
+                        <div style={{marginBottom: '60px'}}/>
+                        <button
+                            onClick={this.onDeleteClick.bind(this)}
+                            className="btn btn-danger"
+                        >
+                            Delete My Account
+                        </button>
+                    </div>
+                )
+            } else {
                 //User is logged in but has no profile
                 dashboardContent = (
-                  <div>
-                      <p className="lead text-muted">Welcome {user.name}</p>
-                      <p>You have not yet setup a profile, please and some info</p>
-                      <Link to="/create-profile" className="btn btn-lg btn-info">
-                          Create profile
-                      </Link>
-                  </div>
+                    <div>
+                        <p className="lead text-muted">Welcome {user.name}</p>
+                        <p>You have not yet setup a profile, please and some info</p>
+                        <Link to="/create-profile" className="btn btn-lg btn-info">
+                            Create profile
+                        </Link>
+                    </div>
                 );
             }
         }
 
         return (
             <div className="dashboard">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                        <h1 className="display-4">Dashboard</h1>
-                        {dashboardContent}
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h1 className="display-4">Dashboard</h1>
+                            {dashboardContent}
+                        </div>
                     </div>
                 </div>
-            </div>
             </div>
         )
     }
@@ -62,4 +81,4 @@ const mapStateToProps = state => ({
     auth: state.auth
 });
 
-export default connect(mapStateToProps, {getCurrentProfile})(Dashboard);
+export default connect(mapStateToProps, {getCurrentProfile, deleteAccount})(Dashboard);
