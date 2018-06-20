57ef90e9deb786eefbed3d1d2a3b010458fb60a7
maksimvorobyov <gtl.vorobyov@gmail.com>
2018-06-20 23:32:43 +0300

    end 73

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -6,11 +6,13 @@ import {
   GET_POSTS,
   GET_POST,
   DELETE_POST,
-  POST_LOADING
+  POST_LOADING,
+  CLEAR_ERRORS
 } from "./types";
 
 //Add Post
 export const addPost = postData => dispatch => {
+  dispatch(clearErrors());
 
   axios
     .post('/api/posts', postData)
@@ -30,7 +32,7 @@ export const addPost = postData => dispatch => {
 };
 
 
-//Get Post
+//Get Posts
 export const getPosts = () => dispatch => {
 
   dispatch(setPostLoading);
@@ -122,9 +124,57 @@ export const removeLike = id => dispatch => {
 
 };
 
+
+//Add Comment
+export const addComment = (postId, commentData) => dispatch => {
+  dispatch(clearErrors());
+  axios
+    .post(`/api/posts/comment/${postId}`, commentData)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    )
+
+};
+
+//Delete Comment
+export const deleteComment = (postId, commentId) => dispatch => {
+
+  axios
+    .delete(`/api/posts/comment/${postId}/${commentId}`)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    )
+
+};
+
 //Set loading state
 export const setPostLoading = () => {
   return {
     type: POST_LOADING
+  }
+};
+
+//Clear errors
+export const clearErrors = () => {
+  return {
+    type: CLEAR_ERRORS
   }
 };
---
+++ client/src/components/post/CommentFeed.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import propTypes from 'prop-types';
+import CommentItem from './CommentItem'
+
+
+class CommentFeed extends Component {
+  render() {
+    const {comments, postId} = this.props;
+    return comments.map(comment => (
+      <CommentItem
+        key={comment._id}
+        comment={comment}
+        postId={postId}
+      />))
+  }
+}
+
+CommentFeed.propTypes = {
+  comments: propTypes.array.isRequired,
+  postId: propTypes.string.isRequired
+};
+
+
+export default CommentFeed;
